d928598e980eb87e75f3cc5b7ca69c52db044cfe
Charles Russell <charles.russell@webteks.com>
2018-07-28 06:59:59 -0400

    Modified README.md with some developer notes of useful and interesting folders in this project. Setup of bitmex stub in exchanges folder.

---
+++ scratch.js
@@ -0,0 +1,19 @@
+const BitMEXClient = require('bitmex-realtime-api');
+// See 'options' reference below
+const client = new BitMEXClient({testnet: true});
+
+client.addStream('XBTUSD', 'trade', () => {});
+setTimeout(() => {
+  console.log('XBTUSD trades during the last few seconds:', client.getTable('trade').XBTUSD);
+}, 1000);
+
+/*
+client.on('initialize', () => {
+  console.log(client.streams);  // Log .public, .private and .all stream names
+});
+
+client.addStream('XBTUSD', 'instrument', function (data, symbol, tableName) {
+  // Do something with the table data...
+  console.log(data);
+});
+*/
